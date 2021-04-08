044f4944e9edb5d5906ae1177a67580efdb48729
Anushka Mathur <33370947+AnushkaMathur@users.noreply.github.com>
2021-04-08 14:06:50 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "ami" {}
+
+variable "type" {}
+
+variable "key" {}
+
+variable "availability_zone1" {}
+variable "availability_zone2" {}
+
+variable "dbname" {}
+
+variable "dbuser" {}
+
+
+variable "dbpass" {}
+
